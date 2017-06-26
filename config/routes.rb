1208dc97d81afbb9d0ae1525f88923f2d7d24cf4
herenow <leonardoshiro@gmail.com>
2017-06-26 20:42:05 -0300

    Fix logout method
    We should not use GET method for the logout endpoint, it may lead to
    problems (such as browsers blindly following this link), it is not
    semantic.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # Authentication
   get '/auth/auth0/callback', to: 'auth0#callback'
-  get '/auth/logout', to: 'auth0#logout'
+  post '/auth/logout', to: 'auth0#logout'
   get '/auth/failure', to: 'auth0#failure'
 
   root 'landing#index'
